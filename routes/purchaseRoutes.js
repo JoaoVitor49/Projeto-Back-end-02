23f51fb53646259fcc9e83aa86e1f2ed79f6a469
João Vitor Pimentel <136565623+JoaoVitor49@users.noreply.github.com>
2025-02-18 07:43:00 -0300

    Update purchaseRoutes.js

--- routes/purchaseRoutes.js
+++ routes/purchaseRoutes.js
@@ -3,9 +3,8 @@ const purchaseController = require('../controllers/purchaseController')
 const {auth} = require('../middlewares/auth')
 const router = express.Router()
 
-router.get('/', auth, purchaseController.showPurchasesPage);
-router.get('/:id', auth, purchaseController.showPurchaseDetails);
-router.post('/', auth, purchaseController.processPurchase);
-router.post('/:id/cancel', auth, purchaseController.cancelPurchase);
+router.get('/', auth, purchaseController.showPurchasesPage)
+router.get('/:id', auth, purchaseController.showPurchaseDetails)
+router.post('/', auth, purchaseController.processPurchase)
 
 module.exports = router;
